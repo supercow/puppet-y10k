776ce0ee61d83d093a863d18546652bf3e9a2a4d
supercow <james.sweeny@gmail.com>
2015-08-20 19:37:18 -0400

    Tests!

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,45 @@
 require 'spec_helper'
 describe 'y10k' do
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('y10k') }
+  let(:facts) do
+    {
+      :concat_basedir => '/var/lib/puppet/concat',
+      :osfamily       => 'RedHat',
+      :path           => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+      :kernel         => 'Linux',
+    }
   end
+
+  context 'with defaults for all parameters' do
+    it { should compile.with_all_deps }
+    it { should contain_class('y10k') }
+
+    it { should contain_package('y10k')}
+  end
+
+  context 'with parameters' do
+    config_file = '/tmp/y10k.conf'
+    prefix = '/var/yum'
+
+    let(:params) {
+      {
+        :config_file => config_file,
+        :prefix      => prefix,
+      }
+    }
+
+    it { should contain_concat(config_file).with({
+        'ensure' => 'present',
+        'name'   => config_file,
+      })
+    }
+
+    it { should contain_concat__fragment('y10k base config').with({
+        'target' => config_file,
+      })
+    }
+
+    it { should contain_concat__fragment('y10k base config').with_content(/pathprefix=#{prefix}/) }
+  end
+
 end
---
+++ spec/defines/repo_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+describe 'y10k::repo', :type => :define do
+
+  let(:facts) do
+    {
+      :concat_basedir => '/var/lib/puppet/concat',
+      :osfamily       => 'RedHat',
+      :path           => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+      :kernel         => 'Linux',
+    }
+  end
+
+  name = 'my_repo'
+  arch = 'x86_64'
+  interval = {
+    'weekday' => 7,
+    'hour'    => 4,
+    'minute'  => 0,
+  }
+  #mirrorlist = 'https://example.com/?mirrors'
+  mirrorlist = 'http://example.com/mirrors'
+  localpath = "centos/7/#{arch}/#{name}"
+
+  params = {
+    :mirrorlist    => mirrorlist,
+    :localpath     => localpath,
+    :arch          => arch,
+    :deleteremoved => true,
+    :interval      => interval,
+  }
+
+
+  let(:title) { name }
+  let(:params) { params }
+
+  config_file = '/etc/y10k.conf'
+  # Why does this work commented out?!
+  #let(:pre_condition) do
+  #  'class { "y10k": config_file => /etc/y10k.conf }'
+  #end
+
+  context 'with basic parameters' do
+    it { should compile.with_all_deps }
+    it { should contain_cron("#{name} repo sync").with(interval) }
+
+    it { should contain_concat__fragment("y10k repo #{name}").with({:target => config_file})}
+
+    it { should contain_concat__fragment("y10k repo #{name}").with_content(/\[#{name}\]/)}
+    non_template_params = [:deleteremoved,:interval]
+    params.each_pair do |k,v|
+      next if non_template_params.include?(k)
+      it { should contain_concat__fragment("y10k repo #{name}").with_content(
+        /#{k.to_s}=#{v}/
+      )}
+    end
+  end
+
+end
